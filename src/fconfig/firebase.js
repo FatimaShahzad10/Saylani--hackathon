9a90fa902b0a26bc5bdb7244176f678bfe7011cf
Fatime Shahzad <fatimashahzad202@gmail.com>
2024-11-02 18:23:07 +0500

    saylani_hackathon

--- src/fconfig/firebase.js
+++ src/fconfig/firebase.js
@@ -14,5 +14,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+ const app = initializeApp(firebaseConfig);
+ export const db = getFirestore(app);
